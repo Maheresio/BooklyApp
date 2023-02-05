8ce58f9b34629ad9f579225d8520c6b89589adcc
Ahmed Maher <zamalek.545r5@gmail.com>
2023-02-05 14:54:46 +0200

    changing theme & adding text to splash view body

--- lib/features/presentation/views/widgets/splash_view_body.dart
+++ lib/features/presentation/views/widgets/splash_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
@@ -7,8 +8,19 @@ class SplashViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image.asset(AssetData.logo),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(AssetData.logo),
+          SizedBox(
+            height: 4.h,
+          ),
+          const Text(
+            'Read free Books',
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/features/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/route_manager.dart';
 
 void main() {
@@ -12,12 +13,16 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  GetMaterialApp(
-      theme: ThemeData().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-      ),
-      home: const SplashView(),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) => GetMaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData.dark().copyWith(
+                scaffoldBackgroundColor: kPrimaryColor,
+              ),
+              home: const SplashView(),
+            ));
   }
 }
-
